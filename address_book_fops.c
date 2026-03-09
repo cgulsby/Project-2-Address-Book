8703a94b7d9b33c224d3011f358c057cfadf786b
Keana De Padua <ksdepadua@cpp.edu>
2026-03-09 00:33:58 -0700

    Redefined load_file() to load in any previous data from a file into an address book (not tested yet...)

--- address_book_fops.c
+++ address_book_fops.c
@@ -11,10 +11,64 @@
 Status load_file(AddressBook *address_book)
 {
 	address_book->fp = fopen(DEFAULT_FILE, "r+");		// If file exists, opens in read/write mode with the file pointer at the beginning of the file
-
+	
 	if(address_book->fp == NULL) {
 		address_book->fp = fopen(DEFAULT_FILE, "w+");	// Creates new, empty file for reading/writing if it didn't previously exist
 	}
+
+	// Data has previously existed, must be loaded into current address book
+	// Assuming csv format = name,phoneNumber,emailAddress,si_no
+
+	FILE *tempFP = address_book->fp;
+
+	char checkEmpty = fgetc(tempFP);
+	if(checkEmpty != EOF) {								// File isn't empty
+		fseek(tempFP, 0, SEEK_SET);						// Makes sure pointer is at beginning of file
+
+		// Variables to hold data that's read in from the file
+		char name[NAME_LEN];
+		char phoneNum1[NUMBER_LEN];
+		char phoneNum2[NUMBER_LEN];
+		char phoneNum3[NUMBER_LEN];
+		char phoneNum4[NUMBER_LEN];
+		char phoneNum5[NUMBER_LEN];
+		char email1[EMAIL_ID_LEN];
+		char email2[EMAIL_ID_LEN];
+		char email3[EMAIL_ID_LEN];
+		char email4[EMAIL_ID_LEN];
+		char email5[EMAIL_ID_LEN];
+
+		int tempCount = 0;	// Counts the number of Contacts read in
+		ContactInfo *tempContactPtr = address_book->list;
+		while (fscanf(tempFP, "%[^,],%[^,],%[^,],%[^,],%[^,],%[^,],%[^,],%[^,],%[^,],%[^,],%[^,],%[^\n]", 
+		name, 
+		phoneNum1, phoneNum2, phoneNum3, phoneNum4, phoneNum5, 
+		email1, email2, email3, email4, email5) == 11) {
+			tempContactPtr += tempCount * (NAME_LEN + (5 * NUMBER_LEN) + (5 * EMAIL_ID_LEN));	// Pointer arithmetic...
+
+			// Add name
+			strcpy(tempContactPtr->name, name);
+			// Add phone numbers
+			strcpy(tempContactPtr->phone_numbers[0], phoneNum1);
+			strcpy(tempContactPtr->phone_numbers[1], phoneNum2);
+			strcpy(tempContactPtr->phone_numbers[2], phoneNum3);
+			strcpy(tempContactPtr->phone_numbers[3], phoneNum4);
+			strcpy(tempContactPtr->phone_numbers[4], phoneNum5);
+			// Add emails
+			strcpy(tempContactPtr->email_addresses[0], email1);
+			strcpy(tempContactPtr->email_addresses[1], email2);
+			strcpy(tempContactPtr->email_addresses[2], email3);
+			strcpy(tempContactPtr->email_addresses[3], email4);
+			strcpy(tempContactPtr->email_addresses[4], email5);
+
+			tempCount++;
+
+			tempContactPtr->si_no = tempCount;
+    	}
+
+		address_book->count = tempCount;
+	}
+
 
 	return e_success;
 }
